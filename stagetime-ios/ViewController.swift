e059b2231233618c1563b7a0b19634aed1abf532
Jamaal Sedayao <jamaal.sedayao@accenture.com>
2018-09-29 13:38:49 -0400

    Initial Commit

---
+++ stagetime-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  stagetime-ios
+//
+//  Created by Jamaal Sedayao on 9/29/18.
+//  Copyright © 2018 Jamaal Sedayao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
